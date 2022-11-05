c2300caa8f64955818969f4788866bb3307f79fc
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-05 09:58:31 +0300

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 /**
  * insertion_sort_list - implementation of insertion sort using c89
- * 
+ *
  * @list: a doubly linked list composed of values
 */
 void insertion_sort_list(listint_t **list)
 {
 	/**
-	 * 1. read the first Value. 
+	 * 1. read the first Value.
 	 * 2. compare to the secnd value.
 	 * 3. if second is less move the value and chnge the pointer.
 	**/
@@ -18,10 +18,13 @@ void insertion_sort_list(listint_t **list)
 	{
 		if(pointer->n > pointer->next->n)
 		{
-			printf("%d\n", pointer->n);
 			temp = pointer->next;
 			pointer->next = temp->next;
+
+			if (temp->next)
+				temp->next->prev = pointer;
 			temp->next = pointer;
+
 			if (pointer->prev)
 			{
 				pointer->prev->next = temp;
@@ -34,11 +37,11 @@ void insertion_sort_list(listint_t **list)
 				pointer->prev = temp;
 				*list = temp;
 			}
+
 			if(temp->prev != NULL && temp->prev->n > temp->n)
 				pointer = temp->prev;
 			else
 				pointer = temp;
-			/*printf("%d\n", pointer->n);*/
 			print_list(*list);
 		}
 		else
