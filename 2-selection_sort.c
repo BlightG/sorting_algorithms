d05d5a3c16216c46d2c514d95623b2a142cd5a09
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-06 20:43:57 +0300

    ch

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 /**
- * 
- * 
+ *
+ *
 */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, temploc;
-        int min, temp, flag;
+	size_t i, j, temploc;
+	int min, temp, flag;
 
 	flag = 0;
-        for(i = 0 ; i < size ; i++)
-        {
-                min = array[i];
-		
-                for(j = i ; j < size ; j++)
-                {
-                        if(min > array[j])
-                        {
-                                min = array[j];
-                                temploc = j;
+	for(i = 0 ; i < size ; i++)
+	{
+		min = array[i];
+
+		for(j = i ; j < size ; j++)
+		{
+			if(min > array[j])
+			{
+				min = array[j];
+				temploc = j;
 				flag = 1;
-                        }
-                }
+			}
+		}
 		if(flag == 1)
-                {
+		{
 			temp = array[temploc];
-                	array[temploc] = array[i];
-                	array[i] = temp;
+			array[temploc] = array[i];
+			array[i] = temp;
 			print_array(array, size);
 		}
 		flag = 0;
-        }
+	}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,45 +1,59 @@
 #include "sort.h"
 /**
  * 
+ * 
 */
-void swap(int *, size_t, int);
-void swap(int *array, size_t i, int pindex)
-{
-        int temp;
-
-        printf("here");
-        array[i] = temp;
-        array[i] = array[pindex];
-        array[pindex] = temp;
-}
 /**
  * 
  * 
+*/
+void partition(int *, size_t, size_t, size_t);
+void swap(int *, size_t, size_t, size_t);
+void partition(int *array, size_t start, size_t end, size_t size)
+{
+	size_t i, pindex;
+	int pivot;
+
+	if(end > start)
+	{
+		pindex = start;
+		pivot = end;
+		for(i = 0 ; i < end ; i++)
+		{
+			if (array[i] < pivot)
+			{
+				swap(array, i, pindex, size);
+				pindex++;
+			}
+		}
+		swap(array, pivot, pindex, size);
+		partition(array, start, pindex - 1, size);
+		partition(array, pindex + 1, end, size);
+	}
+}
+/**
+ * 
+*/
+void swap(int *array, size_t i, size_t pindex, size_t size)
+{
+	int temp;
+
+	if(i > pindex)
+	{	
+		temp = array[i];
+		array[i] = array[pindex];
+		array[pindex] = temp;
+		print_array(array, size);
+	}
+}
+/**
  * 
 */
 void quick_sort(int *array, size_t size)
 {
-        size_t i;
-        int pindex, temp,pivot;
+	size_t start, end;
 
-        pindex = 0;
-        pivot = array[size - 1];
-        for(i = 0 ; i < size - 1 ; i++)
-        {
-                if (array[i] > pivot)
-                {
-                        swap(array, i, pindex);
-                        pindex++;
-                }
-        }
+	start = 0;
+	end = size - 1;
+	partition(array, start, end, size);
 }
-/**
- * choose the last number of the array as the pivot
- * and compare it to the other number unitl you find 
- * the pivot correct spot of the pivot 
- * 
- * make sure to move the smaller numbers to the left
- * and the larger unmbers to the right 
- * 
- * repeat the above  steps until Fully sorted
-*/
