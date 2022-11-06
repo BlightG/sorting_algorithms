b670053d32ba512989940f59142f029e8feed741
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-06 14:02:24 +0300

    sorting modified and add flag

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,23 +5,30 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, temploc;
-	int min, temp;
+        size_t i, j, temploc;
+        int min, temp, flag;
 
-	for(i = 0 ; i < size ; i++)
-	{
-		min = array[i];
-		for(j = i ; j < size ; j++)
-		{
-			if(min > array[j])
-			{
-				min = array[j];
-				temploc = j;
-				temp = array[temploc];
-				array[temploc] = array[i];
-				array[i] = temp;
-			}
+	flag = 0;
+        for(i = 0 ; i < size ; i++)
+        {
+                min = array[i];
+		
+                for(j = i ; j < size ; j++)
+                {
+                        if(min > array[j])
+                        {
+                                min = array[j];
+                                temploc = j;
+				flag = 1;
+                        }
+                }
+		if(flag == 1)
+                {
+			temp = array[temploc];
+                	array[temploc] = array[i];
+                	array[i] = temp;
 		}
-		print_array(array, size);
-	}
+		flag = 0;
+                print_array(array, size);
+        }
 }
