c580a64c38bea4a6de84effc433738978664af28
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-06 13:50:26 +0300

    selection sort day 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,24 +5,23 @@
 */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, temploc;
-        int min, temp;
+	size_t i, j, temploc;
+	int min, temp;
 
-        for(i = 0 ; i < size - 1 ; i++)
-        {
-                min = array[i];
-                for(j = i ; j < size ; j++)
-                {
-                        if(min > array[j])
-                        {
-                                min = array[j];
-                                temploc = j;
-                        }
-                }
-                temp = array[temploc];
-                array[temploc] = array[i];
-                array[i] = temp;
-
-                print_array(array, size);
-        }
+	for(i = 0 ; i < size ; i++)
+	{
+		min = array[i];
+		for(j = i ; j < size ; j++)
+		{
+			if(min > array[j])
+			{
+				min = array[j];
+				temploc = j;
+				temp = array[temploc];
+				array[temploc] = array[i];
+				array[i] = temp;
+			}
+		}
+		print_array(array, size);
+	}
 }
