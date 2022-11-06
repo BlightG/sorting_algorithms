0311334e86abb9a465aa93f064315897d97bfd8d
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-06 21:52:31 +0300

    alomst good

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,10 @@
 #include "sort.h"
 /**
- * 
- * 
-*/
-/**
- * 
- * 
+ *
+ *
 */
 void partition(int *, size_t, size_t, size_t);
-void swap(int *, size_t, size_t, size_t);
+void swap(int *, size_t, size_t);
 void partition(int *array, size_t start, size_t end, size_t size)
 {
 	size_t i, pindex;
@@ -17,37 +13,35 @@ void partition(int *array, size_t start, size_t end, size_t size)
 	if(end > start)
 	{
 		pindex = start;
-		pivot = end;
-		for(i = 0 ; i < end ; i++)
+		pivot = array[end];
+		for(i = start ; i < end ; i++)
 		{
 			if (array[i] < pivot)
 			{
-				swap(array, i, pindex, size);
+				swap(array, i, pindex);
 				pindex++;
 			}
 		}
-		swap(array, pivot, pindex, size);
-		partition(array, start, pindex - 1, size);
+		swap(array, end, pindex);
+		print_array(array, size);
+		if (pindex >= 1)
+			partition(array, start, pindex - 1, size);
 		partition(array, pindex + 1, end, size);
 	}
 }
 /**
- * 
+ *
 */
-void swap(int *array, size_t i, size_t pindex, size_t size)
+void swap(int *array, size_t i, size_t pindex)
 {
 	int temp;
 
-	if(i > pindex)
-	{	
-		temp = array[i];
-		array[i] = array[pindex];
-		array[pindex] = temp;
-		print_array(array, size);
-	}
+	temp = array[i];
+	array[i] = array[pindex];
+	array[pindex] = temp;
 }
 /**
- * 
+ *
 */
 void quick_sort(int *array, size_t size)
 {
