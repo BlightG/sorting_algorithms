fa918eb6cf68453bb9a9b38ad86d08c2a7c5b09d
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-07 12:21:03 +0300

    shell srt

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an int array using the kunuth sequence
+ * 
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, k, temploc, gap;
+	int min, flag, temp;
+
+	flag = gap = 0;
+	while ( size/3 >= gap)
+		gap = gap * 3 + 1;
+
+	while(gap > 0)
+	{
+		for (i = 0 ; i < gap; i++)
+		{
+			for(j = i ; j < size ; j += gap)
+			{
+				min = array[j];
+				for (k = j ; k < size ; k += gap)
+				{
+					if (min > array[k])
+					{
+						min = array[k];
+						temploc = k;
+						flag = 1;
+					}
+				}
+				if (flag == 1)
+				{
+					temp = array[temploc];
+					array[temploc] = array[j];
+					array[j] = temp;
+				}
+				flag = 0;
+			}
+		}
+		gap--;
+		gap /= 3;
+		print_array(array, size);
+	}
+}
