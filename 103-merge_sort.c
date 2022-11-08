4e2ec6fd07a7c1d0a25770092bdc3afaa1d97883
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-08 13:17:31 +0300

    still missing prinitgn sequence

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -19,33 +19,33 @@ int* split(int *array, size_t size)
 	if (size < 2)
 		return (NULL);
 
-        rsize = size / 2;
+	rsize = size / 2;
 	if (size % 2 == 0)
-                lsize = size / 2;
+		lsize = size / 2;
 	else
-                lsize = size / 2 + 1;
+		lsize = size / 2 + 1;
 
-        left = malloc(sizeof(int) * lsize);
-        if(!left)
-                return(NULL);
-        right = malloc(sizeof(int) * rsize);
-        if (!right)
-        {
-                free(right);
-                return(NULL);
-        }
+	left = malloc(sizeof(int) * lsize);
+	if(!left)
+		return(NULL);
+	right = malloc(sizeof(int) * rsize);
+	if (!right)
+	{
+		free(left);
+		return(NULL);
+	}
 
-        for (i = 0 ; i < lsize; i++)
+	for (i = 0 ; i < lsize; i++)
 		left[i] = array[i];
 
-        for (j = 0 , i = lsize ; j < rsize; j++, i++)
+	for (j = 0 , i = lsize ; j < rsize; j++, i++)
 		right[j] = array[i];
 
-        split(left, lsize);
+	split(left, lsize);
 	split(right, rsize);
+
 	merge(array, left, right, lsize, rsize);
 
-	
 	free(right);
 	free(left);
 	return (NULL);
@@ -53,48 +53,47 @@ int* split(int *array, size_t size)
 
 int* merge(int *array, int* left, int* right, size_t lsize, size_t rsize)
 {
-        size_t i, j, k;
+	size_t i, j, k;
 
-        k = i = j = 0;
-        if (array == NULL || left == NULL || right == NULL)
-                return (NULL);
-        if (rsize < 1 || lsize < 1)
-                return (NULL);
+	k = i = j = 0;
+	if (array == NULL || left == NULL || right == NULL)
+		return (NULL);
+	if (rsize < 1 || lsize < 1)
+		return (NULL);
 
-        while(i < lsize && j < rsize)
-        {
-                if(left[i] < right[j])
-                {
-                        array[k] = left[i];
-                        k++;
-                        i++;
-                }
-                else
-                {
-                        array[k] = right[j];
-                        k++;
-                        j++;
-                }
-        }
+	while(i < lsize && j < rsize)
+	{
+		if(left[i] < right[j])
+		{
+			array[k] = left[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			array[k] = right[j];
+			k++;
+			j++;
+		}
+	}
+	while(i < lsize)
+	{
+		array[k] = left[i];
+		k++;
+		i++;
+	}
+	while(j < rsize)
+	{
+		array[k] = right[j];
+		k++;
+		j++;
+	}
 
-        while(i < lsize)
-        {
-                array[k] = left[i];
-                k++;
-                i++;
-        }
-        while(j < rsize)
-        {
-                array[k] = right[j];
-                k++;
-                j++;
-        }
-
-        printf("Merging...\n[left]: ");
-        print_array(left, lsize);
-        printf("[right]: ");
-        print_array(right, rsize);
-        printf("[Done]: ");
-        print_array(array, lsize + rsize);
-        return(array);
+	printf("Merging...\n[left]: ");
+	print_array(left, lsize);
+	printf("[right]: ");
+	print_array(right, rsize);
+	printf("[Done]: ");
+	print_array(array, lsize + rsize);
+	return(array);
 }
